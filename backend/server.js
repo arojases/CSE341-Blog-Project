9b4e91330c44638332812418cbef2df4a6307940
OgunniyiOwamamwen <owamamwen@outlook.com>
2023-03-18 22:39:38 +0100

    connection api

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const port = process.env.PORT || 8080;
 
 dotenv.config();
 app.use(express.json());
-
 
 app.use(bodyParser.json())
   .use((req, res, next) => {
@@ -17,15 +15,10 @@ app.use(bodyParser.json())
     next();
   }).use('/', require('./routes'));
 
-mongoose
-  .connect(process.env.MONGO_API_KEY, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify:true
-  })
-  .then(console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+  mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_API_KEY, () => {
+  console.log("Connected to MongoDB");
+});
 
 app.listen(port, () => {
   console.log("Backend is running.");
